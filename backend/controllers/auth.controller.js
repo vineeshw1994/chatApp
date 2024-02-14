3da82e06396d366862476a7b8437c2b26af49567
vineesh <vineeshw1994@gmail.com>
2024-02-14 09:08:50 +0530

    add the routes and controllers

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,9 +1,16 @@
 export const login = async (req, res) => {
     console.log('this is login user ')
+    res.send('login')
 }
 export const signup = async (req, res) => {
-    console.log('this is login user ')
+    try{
+        const {name,email,mobile,password} = req.body
+
+    }catch(error){
+        console.log(error)
+    }
 }
 export const logout = async (req, res) => {
-    console.log('this is login user ')
+    console.log('this is logout user ')
+    res.send('logout')
 }
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -3,8 +3,8 @@ import { login, signup, logout } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
-router.get('/login',login)
-router.get('/signup',signup)
-router.get('/logout',logout)    
+router.post('/login',login)
+router.post('/signup',signup)
+router.post('/logout',logout)    
 
 export default router; 
